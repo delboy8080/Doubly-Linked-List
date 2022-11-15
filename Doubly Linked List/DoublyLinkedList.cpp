c56fe0500189e6cbffba2bda09131165f61045f8
Derek Flood <derek.flood@dkit.ie>
2022-11-15 10:38:09 +0000

    insert and remove complete

--- Doubly Linked List/DoublyLinkedList.cpp
+++ Doubly Linked List/DoublyLinkedList.cpp
@@ -1,11 +1,36 @@
 // DoublyLinkedList.cpp : Defines the entry point for the console application.
 //
+#include <iostream>
+#include "DList.h"
+using namespace std;
+
+void question1();
 
 
 
 
 int main()
 {
+    question1();
+
     return 0;
 }
 
+void question1()
+{
+    DList<int> lst;
+    int num;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << "Please enter a number" << endl;
+        cin >> num;
+        lst.append(num);
+    }
+    DListIterator<int> iter = lst.getIterator();
+    iter.end();
+    while (iter.isValid())
+    {
+        cout << iter.item()<<", ";
+        iter.previous();
+    }
+}
