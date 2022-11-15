044f288918a2bbd4eef01f1f376615d9af8567b7
Derek Flood <derek.flood@dkit.ie>
2022-11-15 10:54:31 +0000

    Question 2 completed. List Exercises 2

--- Doubly Linked List/DoublyLinkedList.cpp
+++ Doubly Linked List/DoublyLinkedList.cpp
@@ -1,21 +1,59 @@
 // DoublyLinkedList.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include <random>
+#include <time.h>
 #include "DList.h"
 using namespace std;
 
 void question1();
-
+void question2();
 
 
 
 int main()
 {
-    question1();
+    question2();
 
     return 0;
 }
+void question2()
+{
+    srand(time(nullptr));
+    DList<int> nums;
+    for (int i = 0; i < 10; i++)
+    {
+        int num = rand() % 100;
+        nums.append(num);
+    }
+    cout << "[";
+    for (DListIterator<int> iter = nums.getIterator(); iter.isValid(); iter.advance())
+    {
+        cout << iter.item() << ", ";
+    }
+    cout << "]"<<endl;
 
+    // remove uneven numbers
+    for (DListIterator<int> iter = nums.getIterator(); iter.isValid(); )
+    {
+        if (iter.item() % 2 == 1)
+        {
+            iter = nums.remove(iter);
+           
+        }
+        else
+        {
+            iter.advance();
+        }
+    }
+
+    cout << "[";
+    for (DListIterator<int> iter = nums.getIterator(); iter.isValid(); iter.advance())
+    {
+        cout << iter.item() << ", ";
+    }
+    cout << "]";
+}
 void question1()
 {
     DList<int> lst;
